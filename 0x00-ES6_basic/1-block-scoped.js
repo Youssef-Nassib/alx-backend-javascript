1d049bb95a209c0fe2d8cb3b600461a93258eb01
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-09-30 23:08:24 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; //using const so this will be just in if block
+    const task2 = false; //the same for this
   }
 
   return [task, task2];
