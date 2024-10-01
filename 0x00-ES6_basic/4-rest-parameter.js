5feb46efa9d97b14516998b530844dd64d506bf6
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-10-01 00:15:57 +0000

    4. Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments(...args) { // eslint-disable-line no-unused-vars
-	return args.length; // eslint-disable-line no-unused-vars
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
